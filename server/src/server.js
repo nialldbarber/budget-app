1485414ffe814ca4aad0daaefba56b1f0450a9c9
Niall <nialldbarber@gmail.com>
2019-12-10 08:27:10 +0000

    Initial commit

---
+++ server/src/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import { ApolloServer, gql } from 'apollo-server-express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import { importSchema } from 'graphql-import'
+import { resolvers } from '~/schema/resolvers'
+
+const startServer = async () => {
+	const app = express()
+	const port = process.env.port || 7000
+
+	app.use(cors(), bodyParser.json())
+
+	const server = new ApolloServer({
+		typeDefs: importSchema('src/schema/typeDefs.graphql'),
+		resolvers,
+		context: ({ req, res }) => ({
+			req,
+			res
+		})
+	})
+
+	server.applyMiddleware({ app })
+	app.listen(port, () => console.log(`🚀  Schema is ready at http://localhost:${port}${server.graphqlPath}`))
+}
+startServer()
